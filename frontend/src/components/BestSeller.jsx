46497a1302be4aff7e32a0863c6d9c1fd89f2cb2
prateek.chadha <prateek.chadha@tothenew>
2025-01-02 23:32:07 +0530

    created hero section and filters

---
+++ frontend/src/components/BestSeller.jsx
@@ -0,0 +1,35 @@
+import React, { useContext, useEffect, useState } from 'react'
+import { ShopContext } from '../context/ShopContext';
+import Title from './Title';
+import ProductItem from './ProductItem';
+
+const BestSeller = () => {
+
+    const { products } = useContext(ShopContext);
+    const [bestseller, setBestSeller] = useState([]);
+    
+    useEffect(() => {
+        const bestProduct = products.filter(data => data.bestseller);
+        setBestSeller(bestProduct.slice(0, 5));
+    }, [])
+
+
+  return (
+    <div className='my-10'>
+            <div className='text-center py-8 text-3xl'>
+                <Title text1={'BEST'} text2={'SELLERS'} />
+                <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>LoreamAmet voluptua eos et erat eos no amet gubergren dolores duo. Nonumy no elitr tempor.</p>
+            </div>
+
+            {/* Rendering Product */}
+            <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
+                {bestseller.map((item, index) => (
+                    <ProductItem key={index} id={item._id} image={item.image} name={item.name} price={item.price} />
+                ))}
+
+            </div>
+        </div>
+  )
+}
+
+export default BestSeller
