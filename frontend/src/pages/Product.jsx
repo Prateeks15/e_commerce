a90dabc7516bb132e07d6a2a54745af05651afb6
Prateeks15 <prateekchadha15@gmail.com>
2025-01-03 22:34:19 +0530

    cart page implemented and cart functionality

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -2,13 +2,15 @@ import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { ShopContext } from '../context/ShopContext';
 import { assets } from '../assets/assets';
+import RelatedProduct from '../components/RelatedProduct';
 
 const Product = () => {
 
   const { productId } = useParams();
-  const { products } = useContext(ShopContext);
+  const { products, currency, addToCart } = useContext(ShopContext);
   const [productData, setProductData] = useState(false);
   const [image, setImage] = useState('');
+  const [size, setSize] = useState('');
 
   const fetchProductData = async () => {
     const getProductInfo = products.find(item => item._id === productId);
@@ -22,9 +24,9 @@ const Product = () => {
 
   }
 
-  // useEffect(() => {
-  //   fetchProductData()
-  // }, [productId])
+  useEffect(() => {
+    fetchProductData()
+  }, [productId])
 
   console.log(productData, "data= >>");
 
@@ -54,10 +56,46 @@ const Product = () => {
           <img src={assets.star_icon}  alt="" className="w-3 5" />
           <img src={assets.star_icon}  alt="" className="w-3 5" />
           <img src={assets.star_icon}  alt="" className="w-3 5" />
-          <img src={assets.star_icon}  alt="" className="w-3 5" />
+          <img src={assets.star_dull_icon}  alt="" className="w-3 5" />
+          <p className='pl-2'>(122)</p>
+          </div>
+          <p className='mt-5 text-3xl font-medium'>{currency}{productData.price}</p>
+          <p className='mt-5 text-gray-500 md:w-4/5'>{productData.description}</p>
+          <div className='flex flex-col gap-4 my-8'>
+<p>Select Size</p>
+<div className='flex gap-2'>
+{productData.sizes.map((item, index) => (
+<button onClick={() => setSize(item)} className={`${size === item ? 'border-orange-500' : ''} border py-2 px-4 bg-gray-100`} key={index}>{item}</button>
+))}
+</div>
+          </div>
+          <button onClick={() => addToCart(productData._id, size)} className='bg-black text-white px-8 py-3 text-sm active:bg-gray-700'>ADD TO CART</button>
+          <hr className='mt-8 sm:w-4/5' />
+          <div className='text-sm text-gray-500 mt-5 flex flex-col gap-1'>
+<p>100% Original Product</p>
+<p>Cash on Delivery is Available</p>
+<p>Easy Return and Exchange Available</p>
           </div>
         </div>
       </div>
+
+
+      {/* Description & Review Section */}
+
+      <div className='mt-20'>
+<div className='flex'>
+<b className='border px-5 py-3 text-sm'>Description</b>
+<p className='border px-5 py-3 text-sm'>Reviews (122)</p>
+</div>
+<div className='flex flex-col gap-4 border px-6 py-6 text-sm text-gray-500'>
+<p>Loream LoreamLoreamLoreamLoreamLoreamLoreamLoreamLoreamLoreamLoreamLoreamLoreamLorea</p>
+<p>orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy t</p>
+</div>
+      </div>
+
+    {/* Display Related Product */}
+  <RelatedProduct category={productData.category} subCategory={productData.subCategory} />
+
     </div>
   ) :
     <div>
