8d734f4d966539a58d6563270ea342eb39222165
Prateeks15 <prateekchadha15@gmail.com>
2025-01-15 22:37:15 +0530

    add project page admin started

--- admin/src/pages/Add.jsx
+++ admin/src/pages/Add.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import { assets } from '../assets/assets';
+
 
 const Add = () => {
   return (
-    <div>
-        add
-    </div>
+    <form >
+      <div>
+        <p>Upload Image</p>
+        <div>
+          <label htmlFor=''>
+            <img src={assets.upload_area} />
+            <input type='file' id='image1' hidden />
+          </label>
+        </div>
+      </div>
+    </form >
   )
 }
 
